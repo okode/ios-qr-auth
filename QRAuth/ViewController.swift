781d7922e18482a715139bde880f0aeb40e87bd9
Pedro Jorquera <pjorquera@okode.com>
2016-02-12 10:35:27 +0100

    Added initial template

---
+++ QRAuth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRAuth
+//
+//  Created by Pedro Jorquera on 12/2/16.
+//  Copyright © 2016 Okode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
