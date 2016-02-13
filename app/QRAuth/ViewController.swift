4e7872a9f5091a24e8bd6b5009106b9b9413408c
Pedro Jorquera <pjorquera@okode.com>
2016-02-13 02:27:34 +0100

    Added JSON model. Added icons.

--- app/QRAuth/ViewController.swift
+++ app/QRAuth/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFFormDelegate {
 
+    var form: MFForm!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        form = MFForm(controlledBy: self)
+        form.delegate = self
+        
+        let filepath = NSBundle.mainBundle().pathForResource("contact", ofType: "json", inDirectory: "json")
+        do {
+            let contents = try NSString(contentsOfFile: filepath!, usedEncoding: nil) as String
+            form.setForm(contents)
+            form.load()
+        } catch { }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
